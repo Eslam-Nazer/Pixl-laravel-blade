8677e170513bf89bd70c4aad87ea07a08d15725d
Eslam-Nazer <eslam.nazer43@gmail.com>
2026-08-30 21:45:13 +0300

    Fix assets and extract replies

--- routes/web.php
+++ routes/web.php
@@ -16,17 +16,31 @@ Route::get('/feed', function () {
                 'handle' => '@mmich_jj'
             ],
             'content' => <<<str
-             <p>
-                I made this ! <a href="#">#myartwork</a> <a href="#">#pixl</a>
-             </p>
-             <img
-                src="/images/simon-chilling.png"
-                alt="Image post for simon-chilling"
-             />
-          str,
+                 <p>
+                    I made this ! <a href="#">#myartwork</a> <a href="#">#pixl</a>
+                 </p>
+                 <img
+                    src="/images/simon-chilling.png"
+                    alt="Image post for simon-chilling"
+                 />
+              str,
             'likeCount' => 23,
             'replyCount' => 45,
             'repostCount' => 151,
+            'replies' => [
+                [
+                    'profile' => [
+                        'avatar' => '/images/simon-chilling.png',
+                        'displayName' => 'Simon',
+                        'handle' => '@simonswisse'
+                    ],
+                    'content' => '<p>Heh - this is just look like me!</p>',
+                    'postedDateTime' => '1h',
+                    'likeCount' => 52,
+                    'replyCount' => 12,
+                    'repostCount' => 200,
+                ]
+            ]
         ]
     ]));
 
@@ -43,17 +57,31 @@ Route::get('/profile', function () {
                 'handle' => '@mmich_jj'
             ],
             'content' => <<<str
-             <p>
-                I made this ! <a href="#">#myartwork</a> <a href="#">#pixl</a>
-             </p>
-             <img
-                src="/images/simon-chilling.png"
-                alt="Image post for simon-chilling"
-             />
-          str,
+                 <p>
+                    I made this ! <a href="#">#myartwork</a> <a href="#">#pixl</a>
+                 </p>
+                 <img
+                    src="/images/simon-chilling.png"
+                    alt="Image post for simon-chilling"
+                 />
+            str,
             'likeCount' => 23,
             'replyCount' => 45,
             'repostCount' => 151,
+            'replies' => [
+                [
+                    'profile' => [
+                        'avatar' => '/images/simon-chilling.png',
+                        'displayName' => 'Simon',
+                        'handle' => '@simonswisse'
+                    ],
+                    'content' => '<p>Heh - this is just look like me!</p>',
+                    'postedDateTime' => '1h',
+                    'likeCount' => 52,
+                    'replyCount' => 12,
+                    'repostCount' => 200,
+                ]
+            ]
         ]
     ]));
 
